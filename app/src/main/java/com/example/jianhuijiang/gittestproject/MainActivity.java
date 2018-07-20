667160275e2ece5f62710f3b274c620c7b192b38
zhipeng.liu <zhipeng.liu@phicomm.com>
2018-07-20 17:12:11 +0800

    modify hello world's text

--- app/src/main/java/com/example/jianhuijiang/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/jianhuijiang/gittestproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jianhuijiang.gittestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.hello);
+
+        text.setText("");
+
     }
 }
