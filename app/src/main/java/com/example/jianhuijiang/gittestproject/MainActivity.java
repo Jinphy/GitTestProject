02759eb6e1ee6be91816f5b7e9a93548775691b3
luqin.zhou <1065200457@qq.com>
2018-07-20 16:52:05 +0800

    Add a Toast showing

--- app/src/main/java/com/example/jianhuijiang/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/jianhuijiang/gittestproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jianhuijiang.gittestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hahaha", Toast.LENGTH_SHORT).show();
     }
 }
